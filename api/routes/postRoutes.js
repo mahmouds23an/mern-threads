c7fd27967a9255796ccb9647c1b8057df004859f
mahmouds23an <pgemahmoud@gmail.com>
2024-06-06 14:29:37 +0300

    finish get and delete a post functionalities in backend with their routes

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,12 +1,18 @@
 import express from "express";
 
-import { createPost } from "../controllers/postController.js";
+import {
+  createPost,
+  getPost,
+  deletePost,
+} from "../controllers/postController.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 // Routes
 // http://localhost:5000/api/posts/.....
-router.post("/create",protectRoute, createPost);
+router.post("/create", protectRoute, createPost);
+router.get("/:id", protectRoute, getPost);
+router.delete("/:id", protectRoute, deletePost);
 
 export default router;
